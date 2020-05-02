55a0e00a3bf6f4e2f2b55f8661d962ee19e2529b
Justin Collantes <JustinCIS@outlook.com>
2020-05-01 20:08:29 -0400

    Updates

---
+++ precache-manifest.e36c6012e617fe864ed95ac7653c0f1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae0c10c03ab805da3a9a4971482fa561",
+    "url": "/webmd-card-catalog/index.html"
+  },
+  {
+    "revision": "288a4ae2e455e0bed63b",
+    "url": "/webmd-card-catalog/static/css/main.a954079b.chunk.css"
+  },
+  {
+    "revision": "5366a35848a33050397b",
+    "url": "/webmd-card-catalog/static/js/2.16535a8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/webmd-card-catalog/static/js/2.16535a8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "288a4ae2e455e0bed63b",
+    "url": "/webmd-card-catalog/static/js/main.16e694dc.chunk.js"
+  },
+  {
+    "revision": "361e5c78dd3296c5f751",
+    "url": "/webmd-card-catalog/static/js/runtime-main.723acdaf.js"
+  }
+]);
